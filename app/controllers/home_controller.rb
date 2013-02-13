1c1bdb679b4e5f7a852bbb824f6734070d473120
Dave Stys <davestys@gmail.com>
2013-02-12 23:33:53 -0500

    laid out wod counts for each month - on index page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,11 @@ class HomeController < ApplicationController
   	if current_user
   		@total_workouts = current_user.workouts.all.count
   		@num_workouts_in_last_week = current_user.workouts.where('date >= ?', 1.week.ago).count
+
+  		@wods_for_current_year = current_user.workouts.find(:all, :conditions => ["strftime('%Y', date) = '?'", Date.today.year])
+  		@wod_months = @wods_for_current_year.group_by { |w| w.date.beginning_of_month }
   	end
   end
+
+
 end
