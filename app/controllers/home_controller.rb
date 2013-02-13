6aff12df54cc0e6eaf623c6ddfbcbff05fcd712a
Dave Stys <davestys@gmail.com>
2013-02-13 07:27:22 -0500

    moved to pg database for dev

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   		@total_workouts = current_user.workouts.all.count
   		@num_workouts_in_last_week = current_user.workouts.where('date >= ?', 1.week.ago).count
 
-  		@wods_for_current_year = current_user.workouts.find(:all, :conditions => ["strftime('%Y', date) = '?'", Date.today.year])
+  		@wods_for_current_year = current_user.workouts.where("date >= ?", Time.now.at_beginning_of_year)
   		@wod_months = @wods_for_current_year.group_by { |w| w.date.beginning_of_month }
   	end
   end
